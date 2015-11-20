4c61020b62d0bc6660cc2e5c69137d9b581a0b76
Zen Savona <z@zensavona.com>
2015-11-20 16:20:48 +0100

    configure / request funcs

--- lib/ninjaproxies.ex
+++ lib/ninjaproxies.ex
@@ -1,2 +1,27 @@
 defmodule Ninjaproxies do
+  @moduledoc """
+  A very simple interface for programatically dealing with the [NinjaProxies](http://ninjaproxies.com) API
+  """
+
+  @doc """
+  Set the API key for making requests
+
+  ## Example
+    iex(1)> Ninjaproxies.configure("XXXXXXXXXXX")
+    :ok
+  """
+  defdelegate configure(api_key), to: Ninjaproxies.Config, as: :configure
+
+  @doc """
+  Set the API key from the application config or ENV var
+  """
+  defdelegate configure, to: Ninjaproxies.Config, as: :configure
+
+  @doc """
+  Request a list of proxies
+
+  - limit is an integer
+  - options is a map with the following potential members, each a single string or list of strings: :order, :portNum, :type, :prototol, :countryCode, :countryName, :regionName, :speed, :bandwidth, :uptime, :whitelisted, :referrer, :cookie, :get, :post, :google, :facebook, :paypal, :craigslist, :twitter, :youtube, :pinterest, :instagram, :alive, :limit
+  """
+  defdelegate request(options), to: Ninjaproxies.API, as: :request
 end
